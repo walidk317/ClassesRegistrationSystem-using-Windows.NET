8067a3e6f732fbdd5d817d108b0e0734a4298732
walidk317 <144201622+walidk317@users.noreply.github.com>
2023-12-18 12:46:12 +0530

    coursedetail.cs

--- AdmissionForm1/coursedetail.cs
+++ AdmissionForm1/coursedetail.cs
@@ -13,7 +13,7 @@ namespace AdmissionForm1
 {
     public partial class coursedetail : Form
     {
-        string str = "Data Source=WALIDKADRI;Initial Catalog = iiht; Integrated Security = True";
+        string str = "Data Source=YOURSERVERNAME;Initial Catalog = ClassReg; Integrated Security = True";
         SqlConnection con;
         SqlCommand cmd;
         public coursedetail()
